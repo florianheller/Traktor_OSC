9a42ee136c8fe8d5487823c203b26d3f6b6f1801
Florian Heller <florian.heller@uhasselt.be>
2016-11-16 21:22:53 +0100

    Initial Commit

---
+++ DiskPlay Traktor Client/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DiskPlay Traktor Client
+//
+//  Created by Florian Heller on 16/11/2016.
+//  Copyright © 2016 Florian Heller - UHasselt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ DiskPlay Traktor Client/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DiskPlay Traktor Client
+//
+//  Created by Florian Heller on 16/11/2016.
+//  Copyright © 2016 Florian Heller - UHasselt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
